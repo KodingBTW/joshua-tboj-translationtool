36aad813556eb774c26b5a8cab3204f749542917
Rodrigo Muñoz <83840135+KodingBTW@users.noreply.github.com>
2024-11-24 22:22:08 -0300

    Update insert.bat

--- insert.bat
+++ insert.bat
@@ -1,11 +1,11 @@
-echo "Exodus - Questions Inserter"
+echo "Joshua & the Battle of Jerichos - Questions Inserter"
 set romName="Joshua & the Battle of Jericho (USA) (v6.0) (Unl).nes"
 set textFile="Questions.bin"
 set tblFile="encoder.tbl"
 set pointersStartAddress=0x14388
 set textStartAddress=0x1007B
 set headerSize=0x8010
-set textSize=430D
+set textSize=0x430D
 :loop
 	pause
 	HexString -e %textFile% %textStartAddress% %textSize% %pointersStartAddress% %headerSize% %romName% %tblFile%
